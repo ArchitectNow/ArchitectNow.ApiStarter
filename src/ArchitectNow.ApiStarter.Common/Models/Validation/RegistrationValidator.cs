ff3bc97d63394a37dfaed1f1b0133f89b4de7db4
Kevin Grossnicklaus <kvgros@architectnow.net>
2017-10-18 10:47:15 -0500

    Fixed typo

--- src/ArchitectNow.ApiStarter.Common/Models/Validation/RegistrationValidator.cs
+++ src/ArchitectNow.ApiStarter.Common/Models/Validation/RegistrationValidator.cs
@@ -11,7 +11,7 @@ namespace ArchitectNow.ApiStarter.Common.Models.Validation
             RuleFor(vm => vm.Email).NotEmpty().EmailAddress();
             RuleFor(vm => vm.NameFirst).Length(3);
             RuleFor(vm => vm.NameLast).Length(3);
-            RuleFor(vm => vm.Password).Password().WithMessage("Passord must be 4 numbers only");
+            RuleFor(vm => vm.Password).Password().WithMessage("Password must be 4 numbers only");
             
         }   
     }
