ba8e6db3b2e92dc5fd61c7fe05975218bec22cd8
Chris Young <cyoung@architectnow.net>
2018-07-19 12:26:59 -0500

    Updated files

---
+++ src/ArchitectNow.ApiStarter.Api/LoggingExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace ArchitectNow.ApiStarter.Api
+{
+    public static class LoggingExtensions
+    {
+        public static LoggerConfiguration ConfigureLogging(this IConfiguration configuration, LoggerConfiguration loggerConfiguration = null)
+        {
+            var baseDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var logPath = Path.Combine(baseDir ?? string.Empty, "logs");
+            if (!Directory.Exists(logPath))
+            {
+                Directory.CreateDirectory(logPath);
+            }
+
+            var loggingConfiguration = loggerConfiguration ?? new LoggerConfiguration()
+                                           .ReadFrom.Configuration(configuration)
+                                           .WriteTo
+                                           .RollingFile($@"{logPath}\{{Date}}.txt", retainedFileCountLimit: 10, shared: true)
+                                           .WriteTo.Console(theme: AnsiConsoleTheme.Code);
+
+          
+            Log.Write(LogEventLevel.Information, "Logging has started");
+
+            return loggingConfiguration;
+        }
+    }
+}
--- src/ArchitectNow.ApiStarter.Api/Program.cs
+++ src/ArchitectNow.ApiStarter.Api/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
 
 namespace ArchitectNow.ApiStarter.Api
 {
@@ -53,30 +50,6 @@ namespace ArchitectNow.ApiStarter.Api
                 .UseStartup<Startup>();
 
             return builder.Build();
-        }
-    }
-
-    public static class LoggingExtensions
-    {
-        public static LoggerConfiguration ConfigureLogging(this IConfiguration configuration, LoggerConfiguration loggerConfiguration = null)
-        {
-            var baseDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var logPath = Path.Combine(baseDir ?? string.Empty, "logs");
-            if (!Directory.Exists(logPath))
-            {
-                Directory.CreateDirectory(logPath);
-            }
-
-            var loggingConfiguration = loggerConfiguration ?? new LoggerConfiguration()
-                                           .ReadFrom.Configuration(configuration)
-                                           .WriteTo
-                                           .RollingFile($@"{logPath}\{{Date}}.txt", retainedFileCountLimit: 10, shared: true)
-                                           .WriteTo.Console(theme: AnsiConsoleTheme.Code);
-
-          
-            Log.Write(LogEventLevel.Information, "Logging has started");
-
-            return loggingConfiguration;
         }
     }
 }
